f64dc03621934a86041624e50cdf236be4c05d3d
mvv1790 <mvv1790@gmail.com>
2022-09-22 01:24:31 +0000

    Added startGame, nextQuestion and show question functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,50 @@
-const question = document.querySelector('#question');
-const optioins = Array.from(document.querySelectorAll('option_text')); 
-const progressText = document.querySelector('');
-const scoreText = document.querySelector('');
-const progressBarFull = doc.querySelector('');
+const startButton = document.getElementById('begin_btn')
+const questionContainerElement = document.getElementById('question')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.querySelector('.answer_container')
+let shuffledQuestions, currentQuestionIndex
 
-let currentQuestion = {};
-let checkAnswer = true;
-let score = 0;
-let questionCounter = 0;
-let avaliableQuestions = []
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log("started")
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() -  .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function selectAnswer(event) {
+
+}
 
 let questions = [
     {
         question: 'The capital of South Africa is...',
-        optionA: 'Bloemfontein',
-        optionB: 'Cape Town',
-        optionC: 'Pretoria',
-        optionD: 'All of the above',
-        answer: 4,
-    },
-    {
-        question: 'The capital of South Africa is...',
-        optionA: 'Bloemfontein',
-        optionB: 'Cape Town',
-        optionC: 'Pretoria',
-        optionD: 'All of the above',
-        answer: 4,
+        answers: [
+            {text: 'Bloemfontein', correct: false},
+            {text: 'Cape Town', correct: false},
+            {text: 'Pretoria', correct: false},
+            {text: 'All of the above', correct: true}
+        ]
     }
 ]
