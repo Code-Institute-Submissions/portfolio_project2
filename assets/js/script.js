72d03e08c662f48385e03d6379d5b1622f850dfd
mvv1790 <mvv1790@gmail.com>
2022-09-24 09:30:49 +0000

    Added a few questions to the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,5 +87,77 @@ const questions = [
             {text: 'Bloemfontein', correct: false},
             {text: 'All three', correct: true}
         ]
-    }
+    },
+    {
+        question: "The distance between earth and the moon is...",
+        answers: [
+            {text: '756,067 km', correct: false},
+            {text: '384,400 km', correct: true},
+            {text: '1,200,000 km', correct: false},
+            {text: '543,860 km', correct: false}
+        ]
+    },
+    {
+        question: "The biggest tank battle in history is known as...",
+        answers: [
+            {text: 'The Battle of Berlin', correct: false},
+            {text: 'The Battle of France', correct: false},
+            {text: 'The Battle of Kursk', correct: true},
+            {text: 'The Battle of Bulge', correct: false}
+        ]
+    },
+    {
+        question: "The largest animal known to have ever existed is...",
+        answers: [
+            {text: 'Blue whale', correct: false},
+            {text: 'African elephant', correct: false},
+            {text: 'Polar bear', correct: true},
+            {text: 'Southern elephant seal', correct: false}
+        ]
+    },
+    {
+        question: "The largest animal known to have ever existed is...",
+        answers: [
+            {text: 'Blue whale', correct: true},
+            {text: 'African elephant', correct: false},
+            {text: 'Polar bear', correct: false},
+            {text: 'Southern elephant seal', correct: false}
+        ]
+    },
+    {
+        question: "The first person known to have proposed a heliocentric system was...",
+        answers: [
+            {text: 'Al-Battānī', correct: false},
+            {text: 'Nicolaus Copernicus', correct: false},
+            {text: 'Aryabhata', correct: false},
+            {text: 'Aristarchus of Samos', correct: true}
+        ]
+    },
+    {
+        question: "Chief Manuelito was a prominent leader of which Native American tribe?",
+        answers: [
+            {text: 'Cherokee', correct: false},
+            {text: 'Mascalero', correct: false},
+            {text: 'Navajo', correct: true},
+            {text: 'Chiricahua', correct: false}
+        ]
+    },
+    {
+        question: "What was the name of the first Canadian satellite to be launched into space?",
+        answers: [
+            {text: 'Explorer', correct: false},
+            {text: 'Asterix', correct: false},
+            {text: 'Ariel', correct: false},
+            {text: 'Alouette', correct: true}
+        ]
+    },
+    {
+        question: "The Punic Wars was a series of wars fought between...",
+        answers: [
+            {text: 'Rome and Carthage', correct: true},
+            {text: 'Egypt and Seleucid Empire', correct: false},
+            {text: 'Mycenaean Empire and Egypt', correct: false},
+            {text: 'Armenia and Pontus', correct: false}
+        ]
+    },
 ]
